7f6065f1e703113eb769babf8098de7d0c63db38
saraalsukary <saraals6216@gmail.com>
2026-09-18 11:34:15 +0300

    commit

--- front/src/api/apiConfig.ts
+++ front/src/api/apiConfig.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const APIURL = axios.create({
     // baseURL: 'http://127.0.0.1:8000/api',
-    baseURL: 'http://japaneseacademy.jp/api',
+    baseURL: 'https://api.japaneseacademy.jp/api',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
